8d0b114fd52c9a4fcbbd14400396882d17f6e1e7
thalesrmacedo <165821269+thalesrmacedo@users.noreply.github.com>
2026-09-06 23:54:51 -0300

    Create views.py

---
+++ views.py
@@ -0,0 +1,28 @@
+from .models import Task 
+
+def task_list(request):
+    if request.method == 'POST':        
+        title = request.POST.get('title')        
+        
+        if title:            
+            Task.objects.create(title=title)        
+        return redirect('task_list')    
+        
+    tasks = Task.objects.all()    
+
+    return render(        
+        request,        
+        'tasks/task_list.html',        
+        {'tasks': tasks}    
+    ) 
+
+def complete_task(request, task_id):    
+    task = get_object_or_404(Task, id=task_id)    
+    task.completed = not task.completed    
+    task.save()    
+    return redirect('task_list') 
+
+def delete_task(request, task_id):    
+    task = get_object_or_404(Task, id=task_id)    
+    task.delete()    
+    return redirect('task_list')
